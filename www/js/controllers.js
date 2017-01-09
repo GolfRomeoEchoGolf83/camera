5b0ae7fb6adbf65db2e628d41f147735294956fc
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-01-09 10:22:20 +0100

    Creating AppCtrl and PhotosCtrl controllers into controllers.js

---
+++ www/js/controllers.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Greg on 09/01/2017.
+ */
+angular.module('camera.controllers', [])
+
+.controller('AppCtrl', function ($scope) {
+
+})
+
+.controller('PhotosCtrl', function ($scope) {
+
+});

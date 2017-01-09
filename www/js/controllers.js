85477b863a7c5c8b81e4a1ec3382e51a74cc21aa
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-01-09 10:46:48 +0100

    Installing et using Camera plugin with ngCordova

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,5 +8,22 @@ angular.module('camera.controllers', [])
 })
 
 .controller('PhotosCtrl', function ($scope) {
+$scope.takePicture = function () {
+  var options = {
+    quality: 75,
+    destinationType: Camera.DestinationType.DATA_URL,
+    sourceType: Camera.PictureSourceType.CAMERA,
+    allowEdit: true,
+    encodingType: Camera.EncodingType.JPEG,
+    targetWidth: 300,
+    targetHeight: 300,
+    saveToPhotoAlbum: false,
+  };
 
+  $cordovaCamera.getPicture(options).then(function (imageData) {
+    $scope.imgURI = "data:image/jpeg;base64," + imageData;
+  }, function (err) {
+    console.log(err);
+  });
+};
 });

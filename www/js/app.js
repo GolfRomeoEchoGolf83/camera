540f02bfa06d091f64dca4af76ce57f610e97dac
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-01-09 10:20:39 +0100

    Creating Routes into app.js

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'camera-controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -22,3 +22,34 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.config(function ($stateProvider, $urlRouterProvider) {
+  $stateProvider
+    .state('app', {
+      url: '/app',
+      abstract: true,
+      templateUrl: 'templates/menu.html',
+      controller: 'AppCtrl'
+    })
+
+    .state('app.home', {
+      url: '/home',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/home.html'
+        }
+      }
+    })
+
+    .state('app.photos', {
+      url: '/photos',
+      views: {
+        'menuContent':{
+          templateUrl: 'templates/photos.html',
+          controller: 'PhotosCtrl'
+        }
+      }
+    });
+
+  $urlRouterProvider.otherwise('app/home');
+});
